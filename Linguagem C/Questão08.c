3cf2be5f6a9df5f2a2a5fec1bc25f7e87225353b
Alycia Andrade <132929970+alyciandrade@users.noreply.github.com>
2023-05-24 14:23:35 -0300

    Update Questão08.c

--- Linguagem C/Questão08.c
+++ Linguagem C/Questão08.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+//QUESTÃO 8
+ 
+ void main()
+{
+    int num = 0;
+    printf("Insira um numero:\n");
+    scanf("%i", &num);
+ 
+    printf("Voce digitou o numero: %i\n", num);
+}
